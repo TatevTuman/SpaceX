aa9e77685785c08569121d251bef62b08bbc5c22
tatev <tatevtumanyan7@gmail.com>
2020-04-17 00:37:06 +0300

    build done

---
+++ build/precache-manifest.a2f7638179ae8a5a3fb37db8c3400e84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f10fd22cac0a5ac1e0e36875706717",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5719d64c5d54ae5eb0ae",
+    "url": "/static/css/main.2b068653.chunk.css"
+  },
+  {
+    "revision": "bc875b09a4de6c2ce945",
+    "url": "/static/js/2.941a9426.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.941a9426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5719d64c5d54ae5eb0ae",
+    "url": "/static/js/main.2733d35b.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  },
+  {
+    "revision": "a41400afc9b7a001d8f41998bf8ce99e",
+    "url": "/static/media/fonmob.a41400af.jpg"
+  },
+  {
+    "revision": "78a8b48e25a8fae7cc6634a9c0bd3615",
+    "url": "/static/media/siteFon.78a8b48e.jpg"
+  }
+]);
